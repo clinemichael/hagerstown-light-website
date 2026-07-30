9aa2c0308f4812976fe44afa47545bc77a7c52b1
Michael Cline <clinemichael@ymail.com>
2026-07-30 11:05:45 -0400

    Updated about page

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,5 +1,15 @@
 import About from "@/components/About";
+import Section from "@/components/Section";
+import SectionTitle from "@/components/SectionTitle";
 
 export default function AboutPage() {
-  return <About />;
+  return (
+    <Section>
+      <SectionTitle
+        title="About"
+        description="Learn more about Hagerstown Light."
+      />
+      <About />
+    </Section>
+  );
 }
